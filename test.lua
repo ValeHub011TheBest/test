00febe709d90034dcea96dfaf55af52b03f8c7e4
ValeScripts <valegamer2011@gmail.com>
2026-01-26 02:45:40 +0100

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,1 +1,14 @@
-print("il link funziona")
+local StarterGui = game:GetService("StarterGui")
+
+-- Funzione per creare un messaggio a schermo senza console
+local function notify(msg)
+    StarterGui:SetCore("SendNotification", {
+        Title = "TEST VALE";
+        Text = msg;
+        Duration = 10;
+    })
+end
+
+notify("Connessione riuscita! Il link funziona.")
+print("Vale, se leggi questo allora lo script sta girando!")
+
